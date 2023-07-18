3b3ddc7b7257cb0f5023e2f74278b8dc00e450b5
crabbbb <87376058+crabbbb@users.noreply.github.com>
2023-07-18 11:41:20 +0800

    create project

---
+++ DataStructureAss/src/TestRun.java
@@ -0,0 +1,11 @@
+
+/**
+ * @author LOH XIN JIE
+ */
+
+public class TestRun {
+
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO DATA STRUCTURE AND ALGORITHMS ASSIGNMENT PROJECT !!");
+    }
+}
