488e553361f9c8ac1d21a0633e63a254aa236275
crabbbb <87376058+crabbbb@users.noreply.github.com>
2023-08-07 22:44:30 +0800

    idea

--- DataStructureAssignment/src/entity/Programme.java
+++ DataStructureAssignment/src/entity/Programme.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package entity;
 
 /**
- *
- * @author LENOVO
+ * @author LOH XIN JIE
  */
 public class Programme {
 
+    private int programmeId;
 }
